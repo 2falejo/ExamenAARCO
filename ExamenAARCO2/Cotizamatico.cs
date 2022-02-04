ec3c3e777017a9230c0928325b10cdb9559c455b
2falejo <josealejando@hotmail.com>
2022-02-04 13:28:43 -0600

    Agregar archivos de proyecto.

---
+++ ExamenAARCO2/Cotizamatico.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ExamenAARCO2
+{
+    public class Cotizamatico
+    {
+
+        public string NombreCatalogo2 { get; set; } = string.Empty;
+        public string Filtro2 { get; set; } = string.Empty;
+        public int IdAplication2 { get; set; }
+
+        public async Task <String> consulta()
+        {
+
+            var contenido = new contenido {NombreCatalogo=NombreCatalogo2,Filtro=Filtro2,IdAplication=IdAplication2};
+
+
+
+            HttpClient client = new HttpClient();
+
+            string url = "https://apitestcotizamatico.azurewebsites.net/api/catalogos";
+
+            var response = await client.PostAsJsonAsync(url, contenido);
+
+            var responsestring = await response.Content.ReadAsStringAsync();
+
+
+            Console.WriteLine(responsestring);
+
+            return responsestring;
+
+        }
+
+
+    }
+}
---
+++ ExamenAARCO2/contenido.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamenAARCO2
+{
+    public class contenido
+    {
+
+        public string NombreCatalogo { get; set; } = string.Empty;
+        public string Filtro { get; set; } = string.Empty;
+        public int IdAplication { get; set; }
+
+    }
+}
